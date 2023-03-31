fcb98dc588a1d81c6153fa0488da5599aab3bfd1
Chumister <alanchu4432@yahoo.com>
2023-03-30 20:10:45 -0500

    initial

---
+++ src/main/java/NASA/OmegaCentauri/OmegaCentauriApplication.java
@@ -0,0 +1,13 @@
+package NASA.OmegaCentauri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmegaCentauriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmegaCentauriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/NASA/OmegaCentauri/OmegaCentauriApplicationTests.java
@@ -0,0 +1,13 @@
+package NASA.OmegaCentauri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmegaCentauriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
