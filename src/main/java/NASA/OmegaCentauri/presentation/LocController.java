c08c8aa8fef469fb832d10b55ce5f4d8e7d134d2
Chumister <alanchu4432@yahoo.com>
2023-05-06 16:27:29 -0500

    More files

---
+++ src/main/java/NASA/OmegaCentauri/presentation/LocController.java
@@ -0,0 +1,43 @@
+package NASA.OmegaCentauri.presentation;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import NASA.OmegaCentauri.repository.dto.Result;
+import NASA.OmegaCentauri.service.LocService;
+
+import java.util.List;
+
+@RestController
+public class LocController {
+
+    private final LocService locService;
+
+    public LocController(LocService locService) {
+        this.locService = locService;
+    }
+
+    @GetMapping("/searchLocResults")
+    @ApiOperation(value = "Searches for articles matching the search term",
+            notes = "Response may include multiple Result values.",
+            response = Result.class,
+            responseContainer="List")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Result(s) found"),
+            @ApiResponse(code = 404, message = "Result(s) not found")
+    })
+    public List<Result> getResults(@RequestParam(value="q") String query){
+        List<Result> results = locService.getResults(query);
+        if(CollectionUtils.isEmpty(results)){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Result(s) not found.");
+        }
+        return results;
+    }
+
+}
