3e237f0720161d050b326a4543840123810039ee
Chumister <alanchu4432@yahoo.com>
2023-06-10 11:21:38 -0700

    Need to work on organization

--- src/test/java/NASA/OmegaCentauri/repository/LocRepositoryTest.java
+++ src/test/java/NASA/OmegaCentauri/repository/LocRepositoryTest.java
@@ -1,17 +1,7 @@
 package NASA.OmegaCentauri.repository;
 
 import NASA.OmegaCentauri.repository.dto.LocResponse;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-
-public class LocRepositoryTest {
-    import com.fasterxml.jackson.annotation.JsonAlias;
-import org.jointheleague.api.cheetah.Cheetah_Search.repository.dto.LocResponse;
-import org.jointheleague.api.cheetah.Cheetah_Search.repository.dto.Result;
+import NASA.OmegaCentauri.repository.dto.Result;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -25,65 +15,64 @@ import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static reactor.core.publisher.Mono.when;
+import static springfox.documentation.builders.PathSelectors.any;
 
-    class LocRepositoryTest {
+class LocRepositoryTest {
 
-        private LocRepository locRepository;
+    private LocRepository locRepository;
 
-        @Mock
-        WebClient webClientMock;
+    @Mock
+    WebClient webClientMock;
 
-        @Mock
-        WebClient.RequestHeadersUriSpec requestHeadersUriSpecMock;
+    @Mock
+    WebClient.RequestHeadersUriSpec requestHeadersUriSpecMock;
 
-        @Mock
-        WebClient.RequestHeadersSpec requestHeadersSpecMock;
+    @Mock
+    WebClient.RequestHeadersSpec requestHeadersSpecMock;
 
-        @Mock
-        WebClient.ResponseSpec responseSpecMock;
+    @Mock
+    WebClient.ResponseSpec responseSpecMock;
 
-        @Mock
-        Mono<LocResponse> LocResponseMonoMock;
+    @Mock
+    Mono<LocResponse> LocResponseMonoMock;
 
-        @BeforeEach
-        void setUp() {
-            MockitoAnnotations.openMocks(this);
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
 
-            locRepository = new LocRepository(webClientMock);
-        }
-
-        @Test
-        void whenGetResults_thenReturnListOfResults() {
-            //given
-            String query = "Java";
-            LocResponse locResponse = new LocResponse();
-            Result result = new Result();
-            result.setTitle("Java: A Drink, an Island, and a Programming Language");
-            result.setAuthors(Collections.singletonList("AUTHOR"));
-            result.setLink("LINK");
-            List<Result> expectedResults = Collections.singletonList(result);
-            locResponse.setResults(expectedResults);
-
-            when(webClientMock.get())
-                    .thenReturn(requestHeadersUriSpecMock);
-            when(requestHeadersUriSpecMock.uri((Function<UriBuilder, URI>) any()))
-                    .thenReturn(requestHeadersSpecMock);
-            when(requestHeadersSpecMock.retrieve())
-                    .thenReturn(responseSpecMock);
-            when(responseSpecMock.bodyToMono(LocResponse.class))
-                    .thenReturn(LocResponseMonoMock);
-            when(LocResponseMonoMock.block())
-                    .thenReturn(locResponse);
-
-            //when
-            List<Result> actualLocResults = locRepository.getResults(query);
-
-            //then
-            assertEquals(expectedResults, actualLocResults);
-        }
-
+        locRepository = new LocRepository(webClientMock);
     }
+
+    @Test
+    void whenGetResults_thenReturnListOfResults() {
+        //given
+        String query = "Java";
+        LocResponse locResponse = new LocResponse();
+        Result result = new Result();
+        result.setTitle("Java: A Drink, an Island, and a Programming Language");
+        result.setAuthors(Collections.singletonList("AUTHOR"));
+        result.setLink("LINK");
+        List<Result> expectedResults = Collections.singletonList(result);
+        locResponse.setResults(expectedResults);
+
+        when(webClientMock.get())
+                .thenReturn(requestHeadersUriSpecMock);
+        when(requestHeadersUriSpecMock.uri((Function<UriBuilder, URI>) any()))
+                .thenReturn(requestHeadersSpecMock);
+        when(requestHeadersSpecMock.retrieve())
+                .thenReturn(responseSpecMock);
+        when(responseSpecMock.bodyToMono(LocResponse.class))
+                .thenReturn(LocResponseMonoMock);
+        when(LocResponseMonoMock.block())
+                .thenReturn(locResponse);
+
+        //when
+        List<Result> actualLocResults = locRepository.getResults(query);
+
+        //then
+        assertEquals(expectedResults, actualLocResults);
+    }
+
 }
